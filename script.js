3c18cbe320a378ea6add515015f6a7e3f38bf17e
Amir Leto <amirus95@hotmail.com>
2024-04-06 10:33:56 +0200

    fix icons sometimes missing error, icon alignment

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let cityVal = document.getElementById('city');
 let tenDays = document.getElementById('tenDays');
 const cityInfo = document.getElementById('cityInfo');
 
-// comment
-
 const bosniaHerzegovinaCities = [
   { name: 'Sarajevo', latitude: 43.8563, longitude: 18.4131 },
   { name: 'Banja Luka', latitude: 44.7758, longitude: 17.1854 },
@@ -87,43 +85,46 @@ let getWeather = async (event) => {
   let cityValue = cityVal.value || cityParam;
   if (!cityValue) {
     show.innerHTML = `<h3 class="error">Upišite ime grada</h3>`;
-  } else {
-    const searchedCity = bosniaHerzegovinaCities.find((searchElement) => searchElement.name.toLowerCase() === cityValue.toLowerCase());
-    let latitude;
-    let longitude;
-    let country;
-    if (!searchedCity) {
-      let locationAPI = `https://novovrijeme.com/api/v2/location?q=${cityValue}`;
-      const res = await fetch(locationAPI);
-      const locationData = await res.json();
-      const locations = locationData?._embedded?.location;
-      if (!locations?.length) {
-        show.innerHTML = `<h3 class="error">City not found</h3>`;
-        return;
-      }
-      const closestLocation = locations[locations.length - 1];
-      console.log('called API', locationData, closestLocation);
-      latitude = closestLocation.position.lat;
-      longitude = closestLocation.position.lon;
-      country = closestLocation.country.name;
-    } else {
-      latitude = searchedCity.latitude;
-      longitude = searchedCity.longitude;
-      country = 'BA';
+    return;
+  }
+  const searchedCity = bosniaHerzegovinaCities.find((searchElement) => searchElement.name.toLowerCase() === cityValue.toLowerCase());
+  let latitude;
+  let longitude;
+  let country;
+  if (!searchedCity) {
+    let locationAPI = `https://novovrijeme.com/api/v2/location?q=${cityValue}`;
+    const res = await fetch(locationAPI);
+    const locationData = await res.json();
+    const locations = locationData?._embedded?.location;
+    if (!locations?.length) {
+      show.innerHTML = `<h3 class="error">City not found</h3>`;
+      tenDays.innerHTML = '';
+      cityInfo.style.display = 'none';
+      return;
     }
+    const closestLocation = locations[locations.length - 1];
+    console.log('called API', locationData, closestLocation);
+    latitude = closestLocation.position.lat;
+    longitude = closestLocation.position.lon;
+    country = closestLocation.country.name;
+  } else {
+    latitude = searchedCity.latitude;
+    longitude = searchedCity.longitude;
+    country = 'BA';
+  }
 
-    const weatherAPIUrl = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${latitude}&lon=${longitude}`;
-    const weatherRes = await fetch(weatherAPIUrl);
-    const weatherData = await weatherRes.json();
+  const weatherAPIUrl = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${latitude}&lon=${longitude}`;
+  const weatherRes = await fetch(weatherAPIUrl);
+  const weatherData = await weatherRes.json();
 
-    const timeSeries = weatherData.properties.timeseries;
-    const currentDetails = timeSeries[0].data.instant.details;
-    const nextHour = timeSeries[0].data.next_1_hours;
-    const weatherDataByDay = getWeatherDataByDay(timeSeries);
-    console.log(timeSeries, weatherDataByDay);
+  const timeSeries = weatherData.properties.timeseries;
+  const currentDetails = timeSeries[0].data.instant.details;
+  const nextHour = timeSeries[0].data.next_1_hours;
+  const weatherDataByDay = getWeatherDataByDay(timeSeries);
+  console.log(timeSeries, weatherDataByDay);
 
-    cityVal.value = '';
-    show.innerHTML = `
+  cityVal.value = '';
+  show.innerHTML = `
         <h2>${cityValue}, ${country}</h2>
         <div style="display:flex; flex-wrap:wrap; justify-content:center; align-items:center">
           <img src="./svg/${nextHour.summary.symbol_code}.svg" width=64 height=64/>
@@ -132,16 +133,16 @@ let getWeather = async (event) => {
  
         </div>
         `;
-    let tenDaysHtml = ``;
-    for (day of weatherDataByDay) {
-      const date = new Date(day.time);
-      tenDaysHtml += `
+  let tenDaysHtml = ``;
+  for (day of weatherDataByDay) {
+    const date = new Date(day.time);
+    tenDaysHtml += `
      <div style="display: flex; padding: 12px; border-bottom: 1px solid gray; align-items: center">
         <div style="flex: 1">
           <p>${daysOfWeek[date.getDay()]} ${date.getDate()} ${monthInYear[date.getMonth()]} </p>
         </div>
         <div style="flex:1">
-          <div style="display:flex; column-gap:8px; max-width:170px; justify-content:flex-end">
+          <div style="display:flex; column-gap:8px; max-width:170px">
             ${day.quarterOne ? `<img src="./svg/${day.quarterOne}.svg" width=36 height=36>` : '<div style="width:36px";height:36px ></div>'}
             ${day.quarterTwo ? `<img src="./svg/${day.quarterTwo}.svg" width=36 height=36>` : '<div style="width:36px";height:36px ></div>'}
             ${day.quarterThree ? `<img src="./svg/${day.quarterThree}.svg" width=36 height=36>` : '<div style="width:36px";height:36px ></div>'}
@@ -152,37 +153,46 @@ let getWeather = async (event) => {
           <p>${Math.round(day.minTemp)}&#8451/${Math.round(day.maxTemp)}&#8451;</p>
         </div>
      </div>`;
-    }
-    tenDays.innerHTML = tenDaysHtml;
-    cityInfo.style.display = 'block';
   }
+  tenDays.innerHTML = tenDaysHtml;
+  cityInfo.style.display = 'block';
 };
 
 const getWeatherDataByDay = (timeseries) => {
   let tenDaysData = {};
   for (entry of timeseries) {
     const time = new Date(entry.time);
-    const currentDate = time.getDate();
+    const UTCHours = time.getUTCHours();
+    const currentDate = time.getUTCDate();
     const next6Hours = entry.data.next_6_hours?.details;
     const next6HoursSummary = entry.data.next_6_hours?.summary;
 
-    if (!tenDaysData?.[currentDate]) {
-      tenDaysData[currentDate] = { ...entry, minTemp: next6Hours?.air_temperature_min, maxTemp: next6Hours?.air_temperature_max, quarterOne: next6HoursSummary?.symbol_code };
-    } else {
-      if (next6Hours?.air_temperature_min < tenDaysData[currentDate].minTemp) {
-        tenDaysData[currentDate].minTemp = next6Hours?.air_temperature_min;
-      }
-      if (next6Hours?.air_temperature_max > tenDaysData[currentDate].maxTemp) {
-        tenDaysData[currentDate].maxTemp = next6Hours?.air_temperature_max;
-      }
+    if (!tenDaysData[currentDate]) {
+      tenDaysData[currentDate] = { ...entry, minTemp: next6Hours?.air_temperature_min, maxTemp: next6Hours?.air_temperature_max };
+    }
 
-      if (time.getUTCHours() === 6) {
+    switch (UTCHours) {
+      case 0:
+        tenDaysData[currentDate] = { ...entry, minTemp: next6Hours?.air_temperature_min, maxTemp: next6Hours?.air_temperature_max, quarterOne: next6HoursSummary?.symbol_code };
+        break;
+      case 6:
         tenDaysData[currentDate]['quarterTwo'] = next6HoursSummary?.symbol_code;
-      } else if (time.getUTCHours() === 12) {
+        break;
+      case 12:
         tenDaysData[currentDate]['quarterThree'] = next6HoursSummary?.symbol_code;
-      } else if (time.getUTCHours() === 18) {
+        break;
+      case 18:
         tenDaysData[currentDate]['quarterFour'] = next6HoursSummary?.symbol_code;
-      }
+        break;
+      default:
+        break;
+    }
+
+    if (next6Hours?.air_temperature_min < tenDaysData[currentDate]?.minTemp) {
+      tenDaysData[currentDate].minTemp = next6Hours?.air_temperature_min;
+    }
+    if (next6Hours?.air_temperature_max > tenDaysData[currentDate]?.maxTemp) {
+      tenDaysData[currentDate].maxTemp = next6Hours?.air_temperature_max;
     }
   }
 
